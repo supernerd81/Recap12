e81d163000db0c459f37e8e4a6ede1bb6bb921d2
Manuel Simon <info@der-supernerd.de>
2025-02-24 14:24:36 +0100

    Recap 12 start

--- src/main/java/de/supernerd/recap12/Recap12Application.java
+++ src/main/java/de/supernerd/recap12/Recap12Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Recap12Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Recap12Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Recap12Application.class, args);
+    }
 
 }
