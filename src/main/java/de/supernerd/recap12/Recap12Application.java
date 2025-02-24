cb473c85be5e6bbee871d3a5bafe326e23bf2ac1
Manuel Simon <info@der-supernerd.de>
2025-02-24 14:24:01 +0100

    Initial commit

---
+++ src/main/java/de/supernerd/recap12/Recap12Application.java
@@ -0,0 +1,13 @@
+package de.supernerd.recap12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Recap12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Recap12Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/supernerd/recap12/Recap12ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.supernerd.recap12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Recap12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
